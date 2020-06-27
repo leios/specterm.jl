f79888535ccd7529616665fe15d76ee1e015ec13
James Schloss <jrs.schloss@gmail.com>
2020-06-27 16:13:53 -0400

    adding NCurses version of moving character game

--- src/specterm.jl
+++ src/specterm.jl
@@ -1,60 +1,85 @@
 using TextUserInterfaces
 using TextUserInterfaces.NCurses
 
-function main()
-    init_tui()
-    noecho()
-    curs_set(0)
-
-    # set of all windows
-    wins = Window[]
-
-    t = 0
-    l = 0
-
-    # background window and container
-    bg_win,bg_container = create_window_with_container(newopc(top = 0,
-                                                              left = 0,
-                                                              height = 24,
-                                                              width = 80),
-                                                       border = true,
-                                                       title = "")
-
-    # creating a person that moves around
-    person = create_window(newopc(top = t, left = l, height = 1, width = 1),
-                           border=true, title="S")
-
-    push!(wins, bg_win)
-    push!(wins, person)
-
-    #refresh_all_windows()
-    update_panels()
-    doupdate()
-
-    # while jlgetch is looking for a "q" keypress, we constantly refresh window
-    key = jlgetch()
-    while key.value != "q"
-        process_focus(key)
-        key = jlgetch()
-
-        if key.value == "w"
-            t -= 1
-        elseif key.value == "a"
-            l -= 1
-        elseif key.value == "s"
-            t += 1
-        elseif key.value == "d"
-            l += 1
-        end
-
-        move_window(person,t,l)
-
-        #refresh_all_windows()
-        update_panels()
-        doupdate()
-    end
-    destroy_tui()
-
+mutable struct Object
+    x::Int
+    y::Int
+    width::Int
+    height::Int
+    content::String
 end
 
+function render_object(obj)
+    mvprintw(obj.x, obj.y, obj.content)
+end
+
+#function create_border(init_y::Int, init_y::Int, height::Int, width::Int)
+#end
+
+function create_person(init_y::Int, init_x::Int)
+    person = Object(init_y, init_x, 1, 1, "S")
+    return person
+end
+
+function init()
+    init_tui()
+    bg = initscr()
+    start_color()
+    #use_default_colors()
+
+    COLORS = [COLOR_BLACK, COLOR_CYAN, COLOR_MAGENTA, COLOR_RED, COLOR_YELLOW,
+              COLOR_BLUE, COLOR_GREEN, COLOR_WHITE]
+
+    for i in COLORS
+        init_pair(i,i,-1)
+    end
+
+    noecho()
+    #bg.keypad(1)
+    return bg
+end
+
+function kill_curses(bg)
+    #nocbreak()
+    #bg.keypad(0)
+    #echo(0)
+    destroy_tui()
+end
+
+function main()
+    bg = init()
+    curs_set(0)
+
+    person = create_person(1,1)
+    render_object(person)
+
+    mvwprintw(bg, 21, 20, "yo")
+    refresh()
+
+    # Eventually, we need some sort of FPS counter, this will work for now.
+    key = jlgetch()
+    while key.value != "q"
+        if key.value == "w"
+            person.x -= 1
+        elseif key.value == "a"
+            person.y -= 1
+        elseif key.value == "s"
+            person.x += 1
+        elseif key.value == "d"
+            person.y += 1
+        end
+
+        erase()
+        mvprintw(10,10,string(person.x))
+        mvprintw(10,20,string(person.y))
+        render_object(person)
+
+        refresh()
+
+        key = jlgetch()
+        process_focus(key)
+    end
+
+    kill_curses(bg)
+end
 main()
